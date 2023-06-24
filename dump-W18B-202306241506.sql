d779d090571f16c9c55fa7c64dfeb1af9799c110
hessdale <hessdale96@gmail.com>
2023-06-24 15:07:26 -0600

    messing around with arrays in sql

---
+++ dump-W18B-202306241506.sql
@@ -0,0 +1,92 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mariadb_array
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cool_nums`
+--
+
+DROP TABLE IF EXISTS `cool_nums`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cool_nums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `num` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cool_nums`
+--
+
+LOCK TABLES `cool_nums` WRITE;
+/*!40000 ALTER TABLE `cool_nums` DISABLE KEYS */;
+INSERT INTO `cool_nums` VALUES (1,123),(2,1),(3,3),(4,31),(5,3),(6,1),(7,23),(8,12),(9,3),(10,3),(11,15),(12,65),(13,13),(14,654),(15,745),(16,7),(17,124),(18,2),(19,34),(20,3),(21,2764),(22,37),(23,4),(24,23),(25,542),(26,3),(27,42345),(28,1),(29,3),(30,1),(31,23),(32,1),(33,1),(34,23),(35,1);
+/*!40000 ALTER TABLE `cool_nums` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'mariadb_array'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `maria_loop` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `maria_loop`(array_input varchar(5000))
+    MODIFIES SQL DATA
+begin
+	declare single_num varchar(20) default '';
+	declare rows_inserted int default 0;
+	set array_input = replace(array_input,'[','');
+	set array_input = replace(array_input,']','');
+
+	while length(array_input) > 0 do
+		set single_num = 
+			trim(substring_index(array_input,',',1));
+		
+		set array_input =
+			trim(substring(array_input,length(single_num) + 2));
+		
+		INSERT INTO mariadb_array.cool_nums (num) VALUES(single_num);
+		set rows_inserted = rows_inserted + 1;
+	end while;
+	
+	select rows_inserted;
+	commit;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-24 15:06:44
